a01c1213407a1621b194bcb88f4f18f6835cf9a2
medayoubadri <medayoubadri@gmail.com>
2025-04-16 17:41:36 +0100

    Tasks: 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  });
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => {
+      const array = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          array.push(result.value);
+        } else {
+          array.push(result.reason);
+        }
+      });
+      return array;
+    });
 }
