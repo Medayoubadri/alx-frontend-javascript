916770305003b049df8893625d38ef91af332bea
medayoubadri <medayoubadri@gmail.com>
2025-04-16 18:10:58 +0100

    Tasks: 6 -2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
+    signUpUser(String(firstName), String(lastName)),
+    uploadPhoto(String(fileName)),
   ]).then((results) => results.map((res) => {
     if (res.status === 'fulfilled') {
       return {
