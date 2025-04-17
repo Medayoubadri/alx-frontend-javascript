16e62fee4eb6f2edd79130b25d1ba873c4e691d4
medayoubadri <medayoubadri@gmail.com>
2025-04-17 14:23:03 +0100

    Tasks: 9.

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,69 @@
+/* eslint-disable */
+export class ALXClass {
+  constructor(year, location) {
+    if (typeof year !== 'number') {
+      throw new TypeError('Year must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+export class StudentALX {
+  constructor(firstName, lastName, alxClass) {
+    if (!(alxClass instanceof ALXClass)) {
+      throw new TypeError('alxClass must be an instance of ALXClass');
+    }
+    if (typeof firstName !== 'string') {
+      throw new TypeError('First name must be a string');
+    }
+
+    if (typeof lastName !== 'string') {
+      throw new TypeError('Last name must be a string');
+    }
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._alxClass = alxClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get alxClass() {
+    return this.alxClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName}${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
+  }
+}
+
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
+const student1 = new StudentALX('Guillaume', 'Salva', class2020);
+const student2 = new StudentALX('John', 'Doe', class2020);
+const student3 = new StudentALX('Albert', 'Clinton', class2019);
+const student4 = new StudentALX('Donald', 'Bush', class2019);
+const student5 = new StudentALX('Jason', 'Sandler', class2019);
+
+const listOfStudents = [
+  student1,
+  student2,
+  student3,
+  student4,
+  student5,
+];
+
+export default listOfStudents;
