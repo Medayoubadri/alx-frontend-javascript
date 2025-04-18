9ef8a248a448c6afd94b9ffddd2879b104680126
medayoubadri <medayoubadri@gmail.com>
2025-04-18 11:30:43 +0100

    Tasks: 9-revert-back

--- 0x02-ES6_classes/StudentALX.js
+++ 0x02-ES6_classes/StudentALX.js
@@ -1,18 +1,7 @@
 /* eslint-disable no-underscore-dangle */
-import ALXClass from './ALXClass';
 
 export default class StudentALX {
   constructor(firstName, lastName, alxClass) {
-    if (!(alxClass instanceof ALXClass)) {
-      throw new TypeError('alxClass must be an instance of ALXClass');
-    }
-    if (typeof firstName !== 'string') {
-      throw new TypeError('First name must be a string');
-    }
-
-    if (typeof lastName !== 'string') {
-      throw new TypeError('Last name must be a string');
-    }
     this._firstName = firstName;
     this._lastName = lastName;
     this._alxClass = alxClass;
